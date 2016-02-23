cdc1b70021fa733ec1f8da52cc6323e6521eadff
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-02-23 17:19:41 +0000

    Implemented validation of email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,10 @@ class User
   attr_reader :password
   attr_accessor :password_confirmation
   validates_confirmation_of :password
+  validates_format_of :email, as: :email_address
 
   property :id, Serial
-  property :email, String
+  property :email, String, required: true
   property :password_digest, Text
 
 
--- spec/user_management_spec.rb
+++ spec/user_management_spec.rb
@@ -5,20 +5,29 @@ feature 'User sign up' do
     expect(page).to have_content('Welcome, alice@example.com')
     expect(User.first.email).to eq('alice@example.com')
   end
-  
+
   scenario 'requires a matching confirmation password' do
-   expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
-   expect(current_path).to eq('/users')
-   expect(page).to have_content 'Password and confirmation password do not match'
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content 'Password and confirmation password do not match'
   end
 
+  scenario "I can't sign up without an email address" do
+    expect { sign_up(email: nil) }.not_to change(User, :count)
+  end
+
+  scenario "I can't sign up with an invalid email address" do
+    expect { sign_up(email: "invalid@email") }.not_to change(User, :count)
+  end
+
+
   def sign_up(email: 'alice@example.com',
-            password: '12345678',
-            password_confirmation: '12345678')
-  visit '/users/new'
-  fill_in :email, with: email
-  fill_in :password, with: password
-  fill_in :password_confirmation, with: password_confirmation
-  click_button 'Sign up'
+              password: '12345678',
+              password_confirmation: '12345678')
+    visit '/users/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+    click_button 'Sign up'
   end
 end
