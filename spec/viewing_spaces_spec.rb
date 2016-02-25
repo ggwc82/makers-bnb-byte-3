9f8dc0e626a6cb53318ee10b0a6d26c8265ad30d
Finble <elainefinn@hotmail.com>
2016-02-25 18:54:14 +0000

    Links back to homepage - functionality added

--- spec/viewing_spaces_spec.rb
+++ spec/viewing_spaces_spec.rb
@@ -15,4 +15,18 @@ feature 'Viewing spaces' do
   	visit '/'
   	expect(current_path).to eq('/spaces')
   end
+
+  scenario 'I can return to the homepage from the sign up page' do
+  	visit '/'
+  	click_button 'Sign up'
+  	click_button 'Homepage'
+  	expect(current_path).to eq('/spaces')
+  end
+
+  scenario 'I can return to the homepage from the sign in page' do
+  	visit '/'
+  	click_button 'Sign in'
+  	click_button 'Homepage'
+  	expect(current_path).to eq('/spaces')
+  end
 end
