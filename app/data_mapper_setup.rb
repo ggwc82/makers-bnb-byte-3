759c60178ccb8788e5a7aa93a5ada7d67e39f6c9
Finble <elainefinn@hotmail.com>
2016-02-25 15:32:39 +0000

    Updated DataMapper setup in prep for Heroku deployment

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -3,6 +3,6 @@ require 'dm-postgres-adapter'
 require_relative 'models/space'
 require_relative 'models/user'
 
-DataMapper.setup(:default, "postgres://localhost/makersbnb_byte3_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/makersbnb_byte3_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
